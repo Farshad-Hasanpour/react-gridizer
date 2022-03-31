8444b312290db071117fdbc574e00e583c7a656c
Farshad Hasanpour <farshad.hasanpour96@gmail.com>
2022-03-31 20:57:06 +0430

    styles are updated to exclude invalid values and to make sure spacer does not have any background color. col props types are updated to accept number with string type

--- src/components/col/Col.tsx
+++ src/components/col/Col.tsx
@@ -5,6 +5,8 @@ import { AlignSelf } from "../flexTypes";
 import {classNames as $c} from "../../utils/functions";
 
 type Size = number | 'unset' | 'auto' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12';
+type Offset = number | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11';
+type Order = number | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12';
 
 interface ColProps extends React.HTMLAttributes<HTMLElement> {
 	"tag"?: React.ElementType,
@@ -16,16 +18,16 @@ interface ColProps extends React.HTMLAttributes<HTMLElement> {
 	"md"?: Size,
 	"lg"?: Size,
 	"xl"?: Size,
-	"order"?: number,
-	"order-sm"?: number,
-	"order-md"?: number,
-	"order-lg"?: number,
-	"order-xl"?: number,
-	"offset"?: number,
-	"offset-sm"?: number,
-	"offset-md"?: number,
-	"offset-lg"?: number,
-	"offset-xl"?: number,
+	"order"?: Order,
+	"order-sm"?: Order,
+	"order-md"?: Order,
+	"order-lg"?: Order,
+	"order-xl"?: Order,
+	"offset"?: Offset,
+	"offset-sm"?: Offset,
+	"offset-md"?: Offset,
+	"offset-lg"?: Offset,
+	"offset-xl"?: Offset,
 	"align-self"?: AlignSelf,
 	"align-self-sm"?: AlignSelf,
 	"align-self-md"?: AlignSelf,
